f2c86b97e70719a9cd082103a197ee70f6c6ab12
bessie <bc2535@nyu.edu>
2023-07-03 08:26:22 +1200

    gateway failed to send exception to frontend
    05:3-2:1

--- bessie-recruit-dev/gateway-8000/src/main/java/com/bessie/filter/SecurityFilterJWT.java
+++ bessie-recruit-dev/gateway-8000/src/main/java/com/bessie/filter/SecurityFilterJWT.java
@@ -1,29 +1,18 @@
 package com.bessie.filter;
 
-import com.google.gson.Gson;
 import com.bessie.base.BaseInfoProperties;
-import com.bessie.grace.result.GraceJsonResult;
+import com.bessie.exceptions.GraceException;
 import com.bessie.grace.result.ResponseStatusEnum;
-import com.bessie.utils.JWTUtils;
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
@@ -59,8 +48,13 @@ public class SecurityFilterJWT extends BaseInfoProperties implements GlobalFilte
         // 针对指定的url，对ip进行判断拦截，限制访问次数
         // 到达此处表示被拦截
         log.warn("被拦截: SecurityFilterJWT url=" + url);
+
+        //  无法触发全局异常拦截处理，需要手动返回
+        GraceException.doException(ResponseStatusEnum.UN_LOGIN);
         return chain.filter(exchange);
     }
+
+
 
 
     // 过滤器的顺序，数字越小优先级则越大
