4511c7df53da6da09303b071fd7419845bd3c804
bessie <bc2535@nyu.edu>
2023-07-14 10:09:50 +0800

    restrict file size
    09:1-7

--- bessie-recruit-dev/recruit-common/src/main/java/com/bessie/exceptions/GraceExceptionHandler.java
+++ bessie-recruit-dev/recruit-common/src/main/java/com/bessie/exceptions/GraceExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.security.SignatureException;
 import java.util.HashMap;
@@ -36,6 +37,13 @@ public class GraceExceptionHandler {
 //        e.printStackTrace();
 //        return GraceJsonResult.errorMsg(e.getMessage());
 //    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseBody
+    public GraceJsonResult returnMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        e.printStackTrace();
+        return GraceJsonResult.exception(ResponseStatusEnum.FILE_MAX_SIZE_500KB_ERROR);
+    }
 
     @ExceptionHandler(MyCustomException.class)
     @ResponseBody
