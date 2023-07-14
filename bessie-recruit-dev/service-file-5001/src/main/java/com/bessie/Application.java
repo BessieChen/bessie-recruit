daee7f0c74431cd2e3f546e07063a31dd35d190c
bessie <bc2535@nyu.edu>
2023-07-14 09:07:37 +0800

    upload face
    09:1-5

--- bessie-recruit-dev/service-file-5001/src/main/java/com/bessie/Application.java
+++ bessie-recruit-dev/service-file-5001/src/main/java/com/bessie/Application.java
@@ -1,5 +1,6 @@
 package com.bessie;
 
+import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -12,7 +13,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @create: 2023-06-21 16:42
  **/
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })   //@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        SeataAutoConfiguration.class
+})   //@SpringBootApplication
 @EnableDiscoveryClient      // 开启服务注册与发现功能
 public class Application {
     public static void main(String[] args) {
--- bessie-recruit-dev/service-file-5001/src/main/java/com/bessie/controller/HelloController.java
+++ bessie-recruit-dev/service-file-5001/src/main/java/com/bessie/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.bessie.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bessie.grace.result.GraceJsonResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 /**
  * @program: bessie-recruit-dev
@@ -13,8 +16,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("file")
 public class HelloController {
+
+    //www.bessie-recruit.com
+    public static final String host = "http://192.168.31.219:8000/";
+
     @GetMapping("hello")
     public Object hello(){
         return "Hello File Service from bessie";
+    }
+
+    @PostMapping("uploadFace")
+    public GraceJsonResult uploadFace(@RequestParam("file") MultipartFile file,
+                                       @RequestParam("userId") String userId,
+                                       HttpServletRequest request) throws Exception {
+
+        // 获得文件原始名称
+        String filename = file.getOriginalFilename();
+
+        //"abc.123.abc.png"
+        // 根据文件名中最后一个点的位置向后进行截取
+        String suffixName = filename.substring(filename.lastIndexOf("."));
+
+        // 文件新的名称
+        String newFileName = userId + suffixName;
+
+        // 设置文件存储的路径，可以存放在指定的路径中，windows用户需要修改为对应的盘符
+        String rootPath = "/Users/bessie" + File.separator;
+        // 图片存储的完全路径
+        String filePath = rootPath + File.separator + "faceFromBessieRecruit" + File.separator + newFileName;
+
+        File newFile = new File(filePath);
+        if (!newFile.getParentFile().exists()) {
+            // 如果目标文件所在目录不存在，则创建父目录
+            newFile.getParentFile().mkdirs();
+        }
+
+        // 将内存中的文件数据写入到磁盘
+        file.transferTo(newFile);
+
+        return GraceJsonResult.ok();
     }
 }
